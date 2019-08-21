bb1107178f4c061383a480dc7cbb6bdc363e999b
Takuma Osono <Takuma@Takuma-no-MacBook-Air.local>
2019-08-22 02:40:04 +0900

    インデント整理

--- hello.php
+++ hello.php
@@ -1,10 +1,12 @@
 <html>
-<head><title>PHP says hello</title></head>
-<body>
-<b>
-<?php
-print "Hello, World";
-?>
-</b>
-</body>
+    <head>
+        <title>PHP says hello</title>
+    </head>
+    <body>
+        <b>
+            <?php
+            print "Hello, World";
+            ?>
+        </b>
+    </body>
 </html>

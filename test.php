29730a22dfd11d68b9763bb9482d5a7ab3c085c9
Takuma Osono <Takuma@Takuma-no-MacBook-Air.local>
2019-09-09 00:17:57 +0900

    20190908

--- test.php
+++ test.php
@@ -1,14 +1,27 @@
-<code>
-  <<<HTMLBLOCK
-  <html>
-  <head><title>Menu</title></head>
-  <body bgcolor="#fffed9">
-    <h1>Dinner</h1>
-    <u1>
-      <li> Beef Chow-Fun
-      <li> Sauteed Pea Shoots
-      <li> Soy Sauce Noodles
-    </u1>
-  </body>
-  </html>
-  HTMLBLOCK
+<!-- print <<<HTMLBLOCK
+<html>
+<head><title>Menu</title></head>
+<body bgcolor="#fffed9">
+<h1>Dinner</h1>
+<u1>
+<li> Beef Chow-Fun
+<li> Sauteed Pea Shoots
+<li> Soy Sauce Noodles
+</u1>
+</body>
+</html>
+HTMLBLOCK; -->
+<?php
+// $a = ' abc ';
+// print $a;
+// $a_len = strlen($a);
+// print "長さは：$a_len";
+//
+// $b = trim($a);
+// print $b;
+// $b_len = strlen($b);
+// print "長さは：$b_len";
+// print ucwords(strtolower('OSONO TAKUMA'));
+$num = '1234-5678-9101-1213';
+print "Card:XX" . substr($num,-4);
+?>
